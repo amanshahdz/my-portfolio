bf1a753a966671f3056048cd69bc38d5622c7ba5
AMAN KUMAR <aman@timewatchindia.com>
2025-06-13 14:02:13 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/' // 👈 required for GitHub Pages
+  base: '/my-portfolio/'  // 👈 required for GitHub Pages
 });
+
