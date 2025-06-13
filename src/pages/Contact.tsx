de2570215fe933c7e8733ee421b13f211594978b
AMAN KUMAR <aman@timewatchindia.com>
2025-06-13 15:14:22 +0530

    commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,5 @@
 export default function Contact() {
-  return <div className="p-6 text-xl">📫 Contact Me My Contact No is: =9079180508 Okkk</div>;
+  return <div className="p-6 text-xl">📫 Contact Me My Contact No is: =9079180508 Okkk
+  
+  My Name is Gulshan</div>;
 }
